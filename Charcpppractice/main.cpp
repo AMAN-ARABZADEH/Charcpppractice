08aa393e483c44be33d3d58544f9487e73842701
Aman <aman.arabzadeh98@gmail.com>
2022-05-12 11:32:46 +0200

    Initial Commit

---
+++ Charcpppractice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Charcpppractice
+//
+//  Created by Aman arabzadeh on 2022-05-12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
